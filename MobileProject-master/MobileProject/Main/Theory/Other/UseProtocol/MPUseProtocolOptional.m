abd812bbe30057467007bd3eaf4760429cd20b3b
caoshuai <capaipai@sina.com>
2018-11-20 18:03:56 +0800

    TWO

---
+++ MobileProject-master/MobileProject/Main/Theory/Other/UseProtocol/MPUseProtocolOptional.m
@@ -0,0 +1,24 @@
+//
+//  MPUseProtocolOptional.m
+//  MobileProject
+//
+//  Created by wujunyang on 2017/3/10.
+//  Copyright © 2017年 wujunyang. All rights reserved.
+//
+
+#import "MPUseProtocolOptional.h"
+#import "MPOraceDataBase.h"
+#import "MPMSSqlDataBase.h"
+
+@implementation MPUseProtocolOptional
+
+-(void)connetcomWithConnetionProtocol:(id<MPDataBaseConnectionProtocol>) service withIndentifier:(NSString *)indentifier
+{
+    NSLog(@"开始执行MPUseProtocolOptional");
+    [service connect];
+    NSLog(@"结束执行MPUseProtocolOptional");
+}
+
+
+
+@end
